e1477c8f2f9d73f64c85b64029f3d466c8a72cc0
taehyun <xogus5070318@gmail.com>
2024-10-15 01:29:28 +0900

    서비스 구조 변경(주인 라즈베리파이에 다시 퍼블리시)

--- main_service.js
+++ main_service.js
@@ -4,14 +4,45 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const mqtt = require('mqtt');
 const pkgInfo = require('./package.json');
 const Service = require('webos-service');
-const broker = require('./mqtt_method/broker.js');
+const mqtt = require('mqtt');
+const moment = require('moment');
 const service = new Service(pkgInfo.name); // Create service by service name on package.json
 const logHeader = "[" + pkgInfo.name + "]";
+
+
 let cancel = true;
 let active = false;
+
+const BROKER_ADDRESS = 'mqtt://192.168.0.4';  // 브로커 주소 (mqtt://를 붙임)
+const PORT = 1883;  // MQTT 기본 포트
+const TOPICS = ['sensor/temperature', 'sensor/humidity', 'sensor/moisture_of_soil', 'sensor/ph_of_soil', 'sensor/ec_of_soil'];  // 여러 센서의 토픽
+let FARM_ID = 'exampleFarmId';  // 농장 ID
+        // 전역 변수 초기화
+let previousTime = moment();
+//온도
+let temperature = 0;
+let avgTemperature = 0;
+
+//습도
+let humidity = 0;
+let avgHumidity = 0;
+
+//토양 수분
+let moistureOfSoil = 0;
+let avgMoistureOfSoil = 0;
+
+//토양 ph
+let phOfSoil = 0;
+let avgPhOfSoil = 0;
+
+//토양 ec
+let ecOfSoil = 0;
+let avgEcOfSoil = 0;
+
+let cnt = 0;
+let currentTime = moment();
 
 // a method that always returns the same value
 service.register("hello", function(message) {
@@ -35,32 +66,93 @@ service.register("konnichiwa", function(message){
     });
 });
 
+
+
+
+
 service.register("serviceOn", (message) => {
+
     let val = !active;
-    if(active==false){
-        active = true;
-        cancel = false;
+    if(active == false) {
+        active = true; // 서비스 활성화
+        cancel = false; // 서비스 중지 상태 해제
         console.log(logHeader, message);
 
-        let i = 0;
-        let interval = setInterval(()=>{
-            let url = "luna://com.webos.notification/createToast";
-            let params = {
-                message: `Hello! +${++i}`
-            };
-        
-            service.call(url, params, (m2)=>{
-                console.log(logHeader, "SERVICE_METHOD_CALLED:com.webos.notification/createToast");
-            });
+        FARM_ID = message.payload.farmId ? message.payload.farmId : FARM_ID;  // 농장 ID
+        // 전역 변수 초기화
+        previousTime = moment();
+        temperature = 0;
+        avgTemperature = 0;
+        humidity = 0;
+        avgHumidity = 0;
+        moistureOfSoil = 0;
+        avgMoistureOfSoil = 0;
+        phOfSoil = 0;
+        avgPhOfSoil = 0;
+        ecOfSoil = 0;
+        avgEcOfSoil = 0;
+        cnt = 0;
 
-            if(cancel == true) {
-                clearInterval(interval);
+        // MQTT 클라이언트 생성
+        const client = mqtt.connect(BROKER_ADDRESS, { port: PORT });
+
+        // 새로운 메시지가 도착했을 때 호출되는 콜백 함수
+        client.on('message', (topic, message) => {
+            currentTime = moment();  // 현재 시간 업데이트
+
+            // 매 분이 변경될 때마다 카운트 초기화
+            if (previousTime.minute() !== currentTime.minute()) {
+                cnt = 0;  // 카운트 리셋
             }
-        }, 3000);
 
-        broker();
+            cnt++;
+            const msgValue = parseFloat(message.toString());  // 수신한 메시지를 숫자로 변환
 
-        //heartbeat 구독
+            // 토픽에 맞는 데이터 처리
+            switch (topic) {
+                case 'sensor/temperature':
+                    temperature = msgValue;
+                    avgTemperature = cnt === 1 ? temperature : ((avgTemperature * (cnt - 1)) + temperature) / cnt;
+                    break;
+                case 'sensor/humidity':
+                    humidity = msgValue;
+                    avgHumidity = cnt === 1 ? humidity : ((avgHumidity * (cnt - 1)) + humidity) / cnt;
+                    break;
+                case 'sensor/moisture_of_soil':
+                    moistureOfSoil = msgValue;
+                    avgMoistureOfSoil = cnt === 1 ? moistureOfSoil : ((avgMoistureOfSoil * (cnt - 1)) + moistureOfSoil) / cnt;
+                    break;
+                case 'sensor/ph_of_soil':
+                    phOfSoil = msgValue;
+                    avgPhOfSoil = cnt === 1 ? phOfSoil : ((avgPhOfSoil * (cnt - 1)) + phOfSoil) / cnt;
+                    break;
+                case 'sensor/ec_of_soil':
+                    ecOfSoil = msgValue;
+                    avgEcOfSoil = cnt === 1 ? ecOfSoil : ((avgEcOfSoil * (cnt - 1)) + ecOfSoil) / cnt;
+                    break;
+            }
+
+            // 이전 시간을 현재 시간으로 업데이트
+            previousTime = currentTime;
+        });
+
+        // 브로커에 연결되었을 때 구독 설정
+        client.on('connect', () => {
+            console.log('Connected to broker');
+            client.subscribe(TOPICS, (err) => {  // 여러 토픽을 배열로 구독
+                if (err) {
+                    console.error('Failed to subscribe to topics:', err);
+                    message.respond({
+                        returnValue: false,
+                        Response: 'Failed to subscribe to topics'
+                    });
+                } else {
+                    console.log('Subscribed to topics:', TOPICS);
+                }
+            });
+        });
+
+        // heartbeat 구독
         const sub = service.subscribe('luna://com.devmonster.farmos.service/heartbeat', {subscribe: true});
 
         sub.addListener("response", function(msg) {
@@ -77,9 +169,11 @@ service.register("serviceOn", (message) => {
 
     message.respond({
         returnValue: val,
+
         Response: val ? "My service has been started." : "service is already running."
     });
 });
+
 const subscriptions = {};
 let heartbeatinterval;
 let x = 1;
@@ -91,6 +185,7 @@ function createHeartBeatInterval() {
     console.log(logHeader, "create_heartbeatinterval");
     heartbeatinterval = setInterval(function() {
         sendResponses();
+        sendPublishes();
     }, 1000);
 }
 
@@ -101,28 +196,72 @@ function sendResponses() {
     for (const i in subscriptions) {
         if (Object.prototype.hasOwnProperty.call(subscriptions, i)) {
             const s = subscriptions[i];
+            
             s.respond({
                 returnValue: true,
-                event: "beat " + x
+                temperature: temperature.toFixed(1),
+                avgTemperature: avgTemperature.toFixed(1),
+                humidity: humidity.toFixed(1),
+                avgHumidity: avgHumidity.toFixed(1),
+                moistureOfSoil: moistureOfSoil.toFixed(1),
+                avgMoistureOfSoil: avgMoistureOfSoil.toFixed(1),
+                phOfSoil: phOfSoil.toFixed(1),
+                avgPhOfSoil: avgPhOfSoil.toFixed(1),
+                ecOfSoil: ecOfSoil.toFixed(1),
+                avgEcOfSoil: avgEcOfSoil.toFixed(1),
+                currentHour: currentTime.hour()
             });
         }
     }
     x++;
 }
 
+function sendPublishes() {
+    // 퍼블리시할 데이터 생성
+    const dataToPublish = {
+        temperature: temperature.toFixed(1),
+        avgTemperature: avgTemperature.toFixed(1),
+        humidity: humidity.toFixed(1),
+        avgHumidity: avgHumidity.toFixed(1),
+        moistureOfSoil: moistureOfSoil.toFixed(1),
+        avgMoistureOfSoil: avgMoistureOfSoil.toFixed(1),
+        phOfSoil: phOfSoil.toFixed(1),
+        avgPhOfSoil: avgPhOfSoil.toFixed(1),
+        ecOfSoil: ecOfSoil.toFixed(1),
+        avgEcOfSoil: avgEcOfSoil.toFixed(1),
+        currentHour: currentTime.hour()
+    };
+
+    // MQTT 브로커에 퍼블리시 (주제는 `{FARM_ID}/infos`)
+    const topic = `${FARM_ID}/infos`;
+    console.log(topic);
+    const publishClient = mqtt.connect(BROKER_ADDRESS, { port: PORT });
+    publishClient.publish(topic, JSON.stringify(dataToPublish), (err) => {
+        if (err) {
+            console.error(`Failed to publish to topic ${topic}:`, err);
+        } else {
+            console.log(`Published data to topic ${topic}:`, dataToPublish);
+        }
+    });
+}
+
 var heartbeat = service.register("heartbeat");
 heartbeat.on("request", function(message) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat");
-    message.respond({event: "beat"}); // initial response 
-    if (message.isSubscription) { 
-        subscriptions[message.uniqueToken] = message; //add message to "subscriptions" 
+    console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat"); //하트비트 수신
+    message.respond({
+        event: "beat",
+    }); // 처음 응답
+    if (message.isSubscription) {  
+        subscriptions[message.uniqueToken] = message; //메시지 구독
         if (!heartbeatinterval) {
             createHeartBeatInterval();
         }
     } 
 }); 
+
+
 heartbeat.on("cancel", function(message) { 
-    delete subscriptions[message.uniqueToken]; // remove message from "subscriptions" 
+    delete subscriptions[message.uniqueToken]; // 구독 취소
     var keys = Object.keys(subscriptions); 
     if (keys.length === 0) { // count the remaining subscriptions 
         console.log("no more subscriptions, canceling interval"); 
